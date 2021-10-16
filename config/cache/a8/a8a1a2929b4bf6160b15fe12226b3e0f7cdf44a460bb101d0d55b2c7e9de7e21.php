12042618a88b003bb12b287d077fc200d54e10f6
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-10-16 11:41:06 -0300

    add-layout.html

---
+++ config/cache/a8/a8a1a2929b4bf6160b15fe12226b3e0f7cdf44a460bb101d0d55b2c7e9de7e21.php
@@ -0,0 +1,89 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* users/index.html */
+class __TwigTemplate_b9be93bc4f7f50a87145bddb3211edc8149cc610c6ad515ffd3e2ced72e8ed0d extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'content' => [$this, 'block_content'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "layout.hrml";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("layout.hrml", "users/index.html", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 4
+        echo "<ul>
+";
+        // line 5
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["users"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["user"]) {
+            // line 6
+            echo "    <li>";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["user"], "name", [], "any", false, false, false, 6), "html", null, true);
+            echo "</li>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['user'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 8
+        echo "</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "users/index.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  66 => 8,  57 => 6,  53 => 5,  50 => 4,  46 => 3,  35 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "users/index.html", "/var/www/phpMvcSiteCms/src/templates/users/index.html");
+    }
+}
